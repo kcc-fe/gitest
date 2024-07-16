d4c0d4cdee6e0eccde624388cb3c3a081ac7c172
Cheol-Cheol <dream174822@gmail.com>
2024-07-17 00:10:10 +0900

    feat: 게시글 생성 기능 구현 [#10]

---
+++ js/post.js
@@ -0,0 +1,66 @@
+import { savePost } from '../api/post.js';
+
+export default function Post() {
+  this.posts = [];
+
+  this.init = () => {
+    this.initData();
+    this.initEventListeners();
+  };
+
+  this.addPost = (e) => {
+    e.preventDefault();
+
+    const data = {
+      title: document.querySelector('#input-title').value,
+      username: document.querySelector('#input-username').value,
+      content: document.querySelector('#input-content').value,
+    };
+
+    // 폼 유효성 검사
+    if (!this.isPostFormValidate(data)) {
+      alert('모든 항목을 입력해주세요.');
+      return;
+    }
+
+    document
+      .querySelector('#post-list')
+      .insertAdjacentHTML('afterbegin', this.postItemTemplate(data));
+
+    // 로컬 스토리지 저장
+    savePost(data);
+  };
+
+  // 게시판 상태 초기화
+  this.initData = () => {
+    // store불러오기
+  };
+
+  // 게시판 이벤트 초기화
+  this.initEventListeners = () => {
+    document.querySelector('#add-btn').addEventListener('click', this.addPost);
+  };
+
+  // 유효성 판별
+  this.isPostFormValidate = ({ title, username, content }) => {
+    if (
+      title.trim() === '' ||
+      username.trim() === '' ||
+      content.trim() === ''
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  // 게시글 항목 생성
+  this.postItemTemplate = ({ title, username, content }) => {
+    return `<li
+  class="list-group-item d-flex justify-content-between align-items-start"
+  >
+  <div class="w-10 fw-bold">${title}</div>
+  <div style="width: 50%">${content}</div>
+  <div>${username}</div>
+  </li>`;
+  };
+}
